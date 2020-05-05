1fdbcfde68b6305b9dbebe9810901e9463ad836e
ezekiel <florian.vidal@lapiscine.pro>
2020-05-05 11:58:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<body>
+
+    <?php include ('_header.html') ?>
+
+    <main>
+
+    <!-- Carousel -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/cabinet1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/gymnase1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/piscine1.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        <div class="container carousel-text">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="slider_text ">
+                        <h4>Votre centre médical à Cazaux</h4>
+                        <h2>Cabinet de Kinésithérapie</h2>
+                        <a href="#" class="boxed-btn5">Visite virtuelle
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel end -->
+
+    <!-- welcome_clicnic_area_start -->
+    <div class="welcome_clicnic_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="welcome_thumb">
+                        <div class="thumb_1">
+                            <img src="img/mathieu.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="welcome_docmed_info">
+                        <h3>Bienvenue au
+                            <span>Cabinet Cazaux-Lac</span></h3>
+                        <p>L’équipe pluridisciplinaire du Cabinet Cazaux-Lac vous souhaite la bienvenue. Notre pôle de soin regroupe différents professionnels de santé : 
+                            <ul>
+                                <li> <i class="flaticon-verified"></i>Masseur-Kinésithérapeutes</li>
+                                <li> <i class="flaticon-verified"></i>Ostéopathes</li>
+                                <li> <i class="flaticon-verified"></i>Infirmières libérales</li>
+                                <li> <i class="flaticon-verified"></i>Kératothérapeute</li>
+                            </ul>
+                        </p>
+                        <p>
+                            Le cabinet se situe sur la route menant au lac de Cazaux-Sanguinet. Il est accessible aux personnes à mobilité réduite et possède un grand parking privé.
+                            <br>
+                            L’accès aux ambulances est facilité par un arrêt minute en face de l’entrée du cabinet, sans entraver la circulation sur cette route passante qu’est l’accès au lac. Ce qui est un avantage et une tranquillité, et pour les patients, et pour les ambulanciers.
+                            <br>
+                            Le cabinet est équipé d’un grand plateau technique, <a href="balneotherapie.php">d’une balnéothérapie</a> et de 6 box individuels. Moderne et à la pointe de la technologie, nous vous offrons un large panel de <a href="materielphysiotherapie.php">matériel de rééducation et de physiothérapie</a>.
+                            <br>
+                            Des soins et suivis infirmiers sont également réalisés au cabinet et à domicile au quotidien. (lien vers Infirmère).
+                            <br>
+                            <br>
+                            <a href="equipe.php">L’équipe de kinésithérapeutes</a> et <a href="osteopathie.php">ostéopathes</a> vous accueille dans ses locaux sur rdv, du lundi au vendredi de 7h30 à 20h30 ainsi que le samedi matin.
+                            Nous nous déplaçons également au domicile des patients ainsi qu’en structure type EHPAD.
+
+                            English and spanish spoken.
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- welcome_clicnic_area_end -->
+
+    </main>
+
+    <?php include ('_footer.html') ?>
+
+</body>
---
+++ soins.php
@@ -0,0 +1,210 @@
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+
+    <?php include ('_header.html') ?>
+
+
+    <!-- depertment_area_start  -->
+    <div class="depertment_area">
+        <div class="container">
+            <div class="row justify-content-center ">
+                <div class="col-lg-9">
+                    <div class="section_title mb-55 text-center">
+                        <h3>Les soins</h3>
+                    </div>
+                </div>            
+            </div>
+            
+            <div class="dept_main_info white-bg">
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <!-- single_content  -->
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <div class="dept_thumb pl-4">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="dept_info pt-4 pb-4">
+                                    <h3>Les prestations de notre cabinet</h3 >
+                                    <hr>
+                                    <ul>
+                                        <li>Massages</li>
+                                        <li>Rééducation fonctionnelle</li>
+                                        <li>Neurologie</li>
+                                        <li>Traumatologie - Orthopédie - Ondes de chocs</li>
+                                        <li>Rhumatologie</li>
+                                        <li>Rééducation médico chirurgicales diverse</li>
+                                        <li>Massages bien être, relaxation, Californien - Réflexologie</li>
+                                        <li>Kiné du sport</li>
+                                        <li>Kiné respiratoire</li>
+                                        <li>Rééducation cardiovasculaire - BPCO (réseau Aqui Respi, Aqui Muco et Aqui BPCO - réseau de garde</li>
+                                        <li>Pédiatrie - Néonatologie (réseau Mal Position du Pied de l'enfant CHU Bordeaux</li>
+                                        <li>Kiné périnatalité</li>
+                                        <li>Rééducation uro gynécologique - Ano rectale - Sexologie (membre de l'EIRPP - Ecole Internationale de la Rééducation du Plancher Pelvien)</li>
+                                        <li>Rééducation pré parfum, post parfum</li>
+                                        <li>Thérapie manuelle</li>
+                                        <li>DLM (Drainage Lympatique Manuel)</li>
+                                        <li>Physiothérapie</li>
+                                        <li>Strapping - K taping</li>
+                                        <li>Endermologie - Esthétique</li>
+                                        <li>Balnéothérapie</li>
+                                        <li>Rééducation SRO Porteur et Port Porteur epino</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- single_content  -->
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <!-- single_content  -->
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <div class="dept_thumb">
+                                    <img src="img/department/1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="dept_info">
+                                    <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                    <a href="#" class="boxed-btn">Make An Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- single_content  -->
+                    </div>
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                    <!-- single_content  -->
+                    <div class="row align-items-center">
+                        <div class="col-lg-5">
+                            <div class="dept_thumb">
+                                <img src="img/department/1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="dept_info">
+                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                <a href="#" class="boxed-btn">Make An Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single_content  -->
+                    </div>
+                    <div class="tab-pane fade" id="Astrology" role="tabpanel" aria-labelledby="Astrology-tab">
+                        <!-- single_content  -->
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <div class="dept_thumb">
+                                    <img src="img/department/1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="dept_info">
+                                    <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                    <a href="#" class="boxed-btn">Make An Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- single_content  -->
+                    </div>
+                    <div class="tab-pane fade" id="Neuroanatomy" role="tabpanel" aria-labelledby="Neuroanatomy-tab">
+                        <!-- single_content  -->
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <div class="dept_thumb">
+                                    <img src="img/department/1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="dept_info">
+                                    <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                    <a href="#" class="boxed-btn">Make An Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- single_content  -->
+                    </div>
+                    <div class="tab-pane fade" id="Blood" role="tabpanel" aria-labelledby="Blood-tab">
+                    <!-- single_content  -->
+                    <div class="row align-items-center">
+                        <div class="col-lg-5">
+                            <div class="dept_thumb">
+                                <img src="img/department/1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="dept_info">
+                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                <a href="#" class="boxed-btn">Make An Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single_content  -->
+                    </div>
+                  </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- depertment_area_end  -->
+
+    <?php include ('_footer.html') ?>
+
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+    <script src="js/main.js"></script>
+    <script>
+        $('.datepicker').datepicker({
+            iconsLibrary: 'fontawesome',
+            icons: {
+                rightIcon: '<span class="fa fa-calendar"></span>'
+            }
+        });
+
+        $('.timepicker').timepicker({
+            iconsLibrary: 'fontawesome',
+            icons: {
+                rightIcon: '<span class="fa fa-clock-o"></span>'
+            }
+        });
+    $(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
+    </script>
+</body>
+
+</html>
